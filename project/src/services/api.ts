924db52d81b4ce7b8eb2ef1ad595ef2833ee7278
luspi1 <igbabin566@gmail.com>
2023-04-06 22:40:03 +0300

    почти доделана авторизация

--- project/src/services/api.ts
+++ project/src/services/api.ts
@@ -1,9 +1,19 @@
-import axios, {AxiosInstance, AxiosRequestConfig} from 'axios';
+import axios, {AxiosError, AxiosInstance, AxiosRequestConfig, AxiosResponse} from 'axios';
 import {getToken} from './token';
+import {toast} from 'react-toastify';
+import {StatusCodes} from 'http-status-codes';
+
+
+const StatusCodeMapping: Record<number, boolean> = {
+  [StatusCodes.BAD_REQUEST]: true,
+  [StatusCodes.UNAUTHORIZED]: true,
+  [StatusCodes.NOT_FOUND]: true
+};
 
 const BACKEND_URL = 'https://12.react.pages.academy/six-cities-simple';
 const REQUEST_TIMEOUT = 5000;
 
+const shouldDisplayError = (response: AxiosResponse) => StatusCodeMapping[response.status];
 export const createAPI = (): AxiosInstance => {
   const api = axios.create({
     baseURL: BACKEND_URL,
@@ -20,6 +30,17 @@ export const createAPI = (): AxiosInstance => {
 
       return config;
     },
+  );
+
+  api.interceptors.response.use(
+    (response) => response,
+    (error: AxiosError<{ error: string }>) => {
+      if (error.response && shouldDisplayError(error.response)) {
+        toast.warn(error.response.data.error);
+      }
+
+      throw error;
+    }
   );
 
   return api;
--- project/src/store/slices/OffersSlice/offers.selectors.ts
+++ project/src/store/slices/OffersSlice/offers.selectors.ts
@@ -1,20 +1,27 @@
 import {State} from '../../../types/state';
 import {NameSpace, OffersSortMap} from '../../../consts';
+import {createSelector} from '@reduxjs/toolkit';
 
 export const getAllOffers = (state: State) => state[NameSpace.Offers].offers;
-export const getSelectSortOffers = (city?: string) => (state: State) => {
-  const offersOnCity = state[NameSpace.Offers].offers.filter((offer) => offer.city.name.toLowerCase() === city?.toLowerCase());
-  switch (state[NameSpace.Offers].sortItem.sortProperty) {
-    case OffersSortMap[0].sortProperty:
-      return offersOnCity;
-    case OffersSortMap[1].sortProperty:
-      return offersOnCity.sort((a, b) => a.price - b.price);
-    case OffersSortMap[2].sortProperty:
-      return offersOnCity.sort((a, b) => b.price - a.price);
-    case OffersSortMap[3].sortProperty:
-      return offersOnCity.sort((a, b) => b.rating - a.rating);
-  }
-};
+
 export const getActiveSort = ((state: State) => state[NameSpace.Offers].sortItem);
+
+export const getSelectSortOffers = (city?: string) => createSelector(
+  [getAllOffers, getActiveSort],
+  (allOffers, activeSort) => {
+    const offersOnCity = allOffers.filter((offer) => offer.city.name.toLowerCase() === city?.toLowerCase());
+    switch (activeSort.sortProperty) {
+      case OffersSortMap[0].sortProperty:
+        return offersOnCity;
+      case OffersSortMap[1].sortProperty:
+        return offersOnCity.sort((a, b) => a.price - b.price);
+      case OffersSortMap[2].sortProperty:
+        return offersOnCity.sort((a, b) => b.price - a.price);
+      case OffersSortMap[3].sortProperty:
+        return offersOnCity.sort((a, b) => b.rating - a.rating);
+    }
+  }
+);
+
 export const getSelectedOffer = ((state: State) => state[NameSpace.Offers].selectedOfferId);
 export const getApiStatus = ((state: State) => state[NameSpace.Offers].apiStatus);
