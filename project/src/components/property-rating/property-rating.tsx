7e538992d312465f747d7acf91174342d9f443f2
luspi1 <47425618+luspi1@users.noreply.github.com>
2023-04-14 15:04:28 +0300

    8.13. Оптимизируй это (часть 2)

--- project/src/components/property-rating/property-rating.tsx
+++ project/src/components/property-rating/property-rating.tsx
@@ -1,27 +1,15 @@
 import { FC } from 'react';
-import { ONE_POINT } from '../../consts';
+import { pointsToPercent } from '../../utils/utils';
 
 type PropertyRatingProps = {
   point?: number;
 }
-export const PropertyRating: FC<PropertyRatingProps> = ({point}) => {
-
-  const pointsToPercent = (pointAmount?: number) => {
-    if (pointAmount) {
-      const percent = pointAmount * ONE_POINT;
-      return `${percent.toString()}%`;
-    } else {
-      return '';
-    }
-  };
-
-  return (
-    <div className="property__rating rating">
-      <div className="property__stars rating__stars">
-        <span style={{width: pointsToPercent(point)}}></span>
-        <span className="visually-hidden">Rating</span>
-      </div>
-      <span className="property__rating-value rating__value">{point}</span>
+export const PropertyRating: FC<PropertyRatingProps> = ({point}) => (
+  <div className="property__rating rating">
+    <div className="property__stars rating__stars">
+      <span style={{width: pointsToPercent(point)}}></span>
+      <span className="visually-hidden">Rating</span>
     </div>
-  );
-};
+    <span className="property__rating-value rating__value">{point}</span>
+  </div>
+);
